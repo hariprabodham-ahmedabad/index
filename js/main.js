7a9d77d2099b1cabee896466ce59f0db36098d0f
Prashant Vaddoriya <ex_vprashant@harsco.com>
2023-02-18 16:29:33 +0530

    changes added.

--- js/main.js
+++ js/main.js
@@ -10,10 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+
+
 
 
     // Sticky Navbar
@@ -24,8 +22,8 @@
             $('.nav-bar').removeClass('sticky-top');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -35,12 +33,82 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
 
+    $('.sabha').change(function () {
+        debugger
+        if ($('.sabha').val() == 3) {
+            $('.m1').show();
+            $('.m2').show();
+            $('.m3').show();
+            $('.m4').show();
+            $('.n1').hide();
+            $('.n2').hide();
+            $('.n3').hide();
+            $('.nn1').hide();
+            $('.nn2').hide();
+            $('.nn3').hide();
+            $('.nn4').hide();
+        }
+        else if ($('.sabha').val() == 1) {
+            $('.m1').hide();
+            $('.m2').hide();
+            $('.m3').hide();
+            $('.m4').hide();
+            $('.n1').show();
+            $('.n2').show();
+            $('.n3').show();
+            $('.nn1').hide();
+            $('.nn2').hide();
+            $('.nn3').hide();
+            $('.nn4').hide();
+        }
+        else if ($('.sabha').val() == 2) {
+            $('.nn1').show();
+            $('.nn2').show();
+            $('.nn3').show();
+            $('.nn4').show();
+            $('.m1').hide();
+            $('.m2').hide();
+            $('.m3').hide();
+            $('.m4').hide();
+            $('.n1').hide();
+            $('.n2').hide();
+            $('.n3').hide();
+        }
+        else if ($('.sabha').val() == 0) {
+            $('.m1').show();
+            $('.m2').show();
+            $('.m3').show();
+            $('.m4').show();
+            $('.n1').show();
+            $('.n2').show();
+            $('.n3').show();
+            $('.nn1').show();
+            $('.nn2').show();
+            $('.nn3').show();
+            $('.nn4').show();
+        }
+        else {
+            $('.m1').hide();
+            $('.m2').hide();
+            $('.m3').hide();
+            $('.m4').hide();
+            $('.n1').hide();
+            $('.n2').hide();
+            $('.n3').hide();
+            $('.nn1').hide();
+            $('.nn2').hide();
+            $('.nn3').hide();
+            $('.nn4').hide();
+        }
 
-    
+    });
+
+
+
 })(jQuery);
 
